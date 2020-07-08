129c727d9c7753555e8c07fdf468eb6b14e1654c
Gaston <gastonaguirre19972@gmail.com>
2020-07-07 21:36:10 -0300

    Se agregaron paginas de area

--- forms/contact.php
+++ forms/contact.php
@@ -7,13 +7,18 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+
+  $receiving_email_address = 'gastonaguirre19972@gmail.com';
+
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
+
+
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
@@ -23,15 +28,15 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  // // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  // /*
+  // $contact->smtp = array(
+  //   'host' => 'example.com',
+  //   'username' => 'example',
+  //   'password' => 'pass',
+  //   'port' => '587'
+  // );
+  // */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
